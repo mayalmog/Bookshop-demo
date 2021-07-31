93c65583402890c4b9012a77cfe28a9cafe0ad21
mayalmog <55530459+mayalmog@users.noreply.github.com>
2021-07-31 16:18:35 +0300

    Add files via upload

---
+++ js/book-controller.js
@@ -0,0 +1,155 @@
+'use strict'
+
+var gRatingCounter = 0;
+
+function onInit() {
+    renderBooks();
+}
+
+function renderBooks() {
+    var books = getBooks();
+    console.log(books);
+    var strHtmls = books.map(function (book) {
+        return `
+        <tr>
+        <td>${book.id}</td>
+        <td>${book.name}</td>
+        <td><img class = "book-img" src="${book.imgUrl}" alt="cover-img"></td>
+        <td>${book.price}₪</td>
+        <td><button class = "read-btn btn"  onclick="onReadBook('${book.id}', event)">Read</button></td>
+        <td><button class = "update-btn btn" onclick="onUpdateBook('${book.id}', event)">Update</button></td>
+        <td><button class = "delete-btn btn" onclick="onDeleteBook('${book.id}', event)">Delete</button></td>
+        <td>
+        <span class= "average-rating"> ${getAveRatingById(book.id)}</span>
+        <br>${(book.ratings.length)} ratings</td>
+    </tr>
+        `
+    })
+    document.querySelector('.books-container').innerHTML = strHtmls.join('')
+}
+
+function onDeleteBook(bookId, ev) {
+    console.log('Deleting: ', bookId);
+    ev.stopPropagation();
+    if (confirm('Confirm to delete Book')) {
+        deleteBook(bookId);
+        renderBooks();
+    }
+}
+
+function onUpdateBook(bookId, ev) {
+    console.log('Updating: ', bookId);
+    ev.stopPropagation();
+    var newPrice = +prompt('Enter new book price in ₪: ');
+    updateBook(bookId, newPrice);
+    renderBooks();
+}
+
+
+function onReadBook(bookId, ev) {
+
+    ev.stopPropagation();
+
+    var book = getBookById(bookId)
+    console.log('book from onReadBook', book)
+    var elModal = document.querySelector('.modal')
+
+    var HTMLStr = ` <img class="modal-img" src="${book.imgUrl}" alt="cover-img">
+        <h5>${book.name}</h5>
+        <h6>${book.price}₪</h6>
+        <p>${book.desc}</p>
+        <button class="rate-minus modal-btn btn" onclick="onMinusRating()">-</button>
+        <span class="rating-display">${gRatingCounter}</span>
+        <button class="rate-plus modal-btn btn" onclick="onPlusRating()">+</button>
+        <button class="modal-btn btn" onclick="onAddRating('${book.id}')">Send Rating</button>
+        <button class="modal-btn btn" onclick="onCloseModal()">Close</button>
+        `
+
+    elModal.innerHTML = HTMLStr;
+    elModal.hidden = false;
+}
+
+function onCloseModal() {
+    gRatingCounter = 0;
+    renderBooks();
+    document.querySelector('.modal').hidden = true;
+}
+
+function onPlusRating() {
+    if (gRatingCounter < 10) {
+        gRatingCounter++;
+    } else {
+        return;
+    }
+    document.querySelector('.rating-display').innerHTML = gRatingCounter;
+
+}
+
+function onMinusRating() {
+    if (gRatingCounter > 0) {
+        gRatingCounter--;
+    } else {
+        return;
+    }
+    document.querySelector('.rating-display').innerHTML = gRatingCounter;
+
+}
+
+function onAddRating(bookId) {
+    var RatingCounter = gRatingCounter;
+    console.log(RatingCounter);
+    addBookRating(bookId, RatingCounter);
+}
+
+
+function onAddBook() {
+
+    var elModal = document.querySelector('.add-new-book')
+
+    elModal.hidden = false;
+}
+
+
+function onAddNewBook() {
+    var elBookTitle = document.querySelector('[name=newAddBookTitle]');
+    var elBookPrice = document.querySelector('[name=newAddBookPrice]');
+    var elBookImg = document.querySelector('[name=newAddBookImg]');
+    var bookTitle = elBookTitle.value;
+    var bookPrice = elBookPrice.value;
+    if (!bookTitle) return;
+
+    if (bookImg) {
+        var bookImg = elBookImg.value;
+        console.log(bookImg);
+        createBook(bookTitle, bookPrice, bookImg);
+    }
+    else {
+        console.log(bookImg);
+        createBook(bookTitle, bookPrice);
+    }
+
+    renderBooks()
+}
+
+function onCloseNewBookModal() {
+
+    renderBooks();
+    document.querySelector('.add-new-book').hidden = true;
+}
+
+function onSetSort(SortBy) {
+    console.log('Sorting by:', SortBy);
+    setSortBy(SortBy);
+    renderBooks();
+}
+
+function onBackPage() {
+    backPage();
+    renderBooks();
+
+}
+
+function onNextPage() {
+    nextPage();
+    renderBooks();
+}
---
+++ js/book-service.js
@@ -0,0 +1,145 @@
+'use strict'
+
+var gBooks = [];
+var gSortBy = 'title';//either title or price
+const PAGE_SIZE = 3;
+var gPageIdx = 0;
+
+_createBooks();
+
+function createBook(title, price, imgUrl = './img/random-book.jfif') {
+    var book = {
+        id: _makeId(),
+        name: title,
+        price: price,
+        imgUrl: imgUrl,
+        desc: _makeLorem(),
+        ratings: [5]
+    }
+    gBooks.unshift(book);
+    _saveBooksToStorage()
+}
+
+function _createBooks() {
+    var books = loadFromStorage('booksDB')
+    if (books && books.length) {
+        gBooks = books;
+    } else {
+        createBook('Sapiens', 80.50, './img/sapiens.jpg')
+        createBook('Light on Yoga', 100.50, './img/light-on-yoga.jpg')
+        createBook('Thinking, Fast and Slow', 75.50, './img/thinking.jpg')
+    }
+
+}
+
+function _saveBooksToStorage() {
+    saveToStorage('booksDB', gBooks)
+}
+
+
+function setSortBy(SortBy) {
+    gSortBy = SortBy;
+}
+
+
+function getBooks() {
+    var books = getBooksForPage();
+
+    //sort
+    switch (gSortBy) {
+        case 'price':
+            books.sort((a, b) => (a.price >= b.price) ? 1 : -1);
+            break;
+        case 'title':
+            books.sort((a, b) => (a.name >= b.name) ? 1 : -1);
+            break;
+
+    }
+    //send arranged array to rederBooks on controller:
+    return books;
+}
+
+function _makeId(length = 3) {
+    var txt = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return txt;
+}
+
+function _makeLorem(size = 100) {
+    var words = ['The sky', 'above', 'the port', 'was', 'the color of television', 'tuned', 'to', 'a dead channel', '.', 'All', 'this happened', 'more or less', '.', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', '.', 'It', 'was', 'a pleasure', 'to', 'burn'];
+    var txt = '';
+    while (size > 0) {
+        size--;
+        txt += words[Math.floor(Math.random() * words.length)] + ' ';
+    }
+    return txt;
+}
+
+function deleteBook(bookId) {
+    var idx = gBooks.findIndex(function (book) {
+        return book.id === bookId
+    })
+    gBooks.splice(idx, 1);
+    _saveBooksToStorage();
+}
+
+function updateBook(bookId, newPrice) {
+    var book = gBooks.find(function (book) {
+        return book.id === bookId;
+    })
+    book.price = newPrice;
+    _saveBooksToStorage();
+}
+
+function getBookById(bookId) {
+    var book = gBooks.find(function (book) {
+        return bookId === book.id
+    })
+    return book
+}
+
+function getAveRatingById(bookId) {
+    var book = getBookById(bookId);
+    var bookRatings = book.ratings;
+    var ratingsSum = bookRatings.reduce((a, b) => a + b, 0);
+    var averageRating = Math.floor(ratingsSum / (bookRatings).length);
+    return averageRating
+}
+
+function addBookRating(bookId, RatingCounter) {
+    console.log('trying to add book rating')
+    var book = getBookById(bookId);
+
+    console.log('Rating counter' + RatingCounter);
+    book.ratings.push(RatingCounter);
+
+    book.rating = getAveRatingById(bookId);
+    _saveBooksToStorage();
+}
+
+function nextPage() {
+    gPageIdx++;
+    if (gPageIdx * PAGE_SIZE >= gBooks.length) {
+        gPageIdx = 0;
+    }
+}
+
+function backPage() {
+    if (gPageIdx === 0) {
+        return;
+    } else {
+        gPageIdx--;
+    }
+
+    if (gPageIdx * PAGE_SIZE >= gBooks.length) {
+        gPageIdx = 0;
+    }
+}
+
+function getBooksForPage() {
+    var startIdx = gPageIdx * PAGE_SIZE;
+    return gBooks.slice(startIdx, startIdx + PAGE_SIZE)
+}
